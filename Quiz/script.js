79d45d994e2c34f0280f13e82b284ba67653f940
mathscape-vr <mathscape@protonmail.com>
2024-05-21 01:46:49 +0000

    Initial commit add missing files

---
+++ Quiz/script.js
@@ -0,0 +1,20 @@
+function checkAnswers() {
+    const quiz = document.forms['math-quiz'];
+    let score = 0;
+
+    const answers = {
+        q1: '1',
+        q2: '210',
+        q3: '37/60',
+        q4: 'b < a < c'
+    };
+
+    for (let question in answers) {
+        const selected = quiz[question].value;
+        if (selected === answers[question]) {
+            score++;
+        }
+    }
+
+    document.getElementById('score').value = score;
+}
